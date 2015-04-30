07b914ac8c70e32c20bc0c17586c8487889089b1
Damla Ozgen <damlaozgen999@gmail.com>
2015-04-30 11:46:24 +0300

    resolve conflict

--- app/src/main/java/me/sunapp/view/MyEventsPage.java
+++ app/src/main/java/me/sunapp/view/MyEventsPage.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import me.sunapp.R;
+import me.sunapp.client.SUNClient;
 
 
 // myevents de  eventlerlistview şeklinde olucak. her bir event create edildiğinde listede
